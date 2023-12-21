11007e9b41636bcfd0c8a3b569588daaf1a22305
joannaking <48340120+joannaking@users.noreply.github.com>
2023-12-22 10:52:47 +1300

    Update streamlit_app.py
    add text choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ streamlit.text(fruityvice_response)
 # New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-import requests
+# import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # # Just writes the data to the screen
 # streamlit.text(fruityvice_response.json())
@@ -45,11 +45,18 @@ import requests
 # # write your own comment - what does this do?
 # streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+# import requests
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
-# take the json version of the response and normalize ti
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+# # take the json version of the response and normalize ti
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# # output it the screen as a table
+# streamlit.dataframe(fruityvice_normalized)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
